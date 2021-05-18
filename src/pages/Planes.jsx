9ff3bedfe22956d18b975bfcb682f6b72e2635eb
danieldmngz96 <danarturodo@hotmail.com>
2021-05-18 11:09:52 -0500

    planes 10%
    tengo algunos problemas con planess

---
+++ src/pages/Planes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+//import PriceCard from '../components/PriceCard';
+//import Hero from '../containers/Hero';
+//import '../styles/pages/home.css';
+
+const Planes = () => {
+    return (
+       <h1>Solo te falta un Paso <br/> 
+       para adquirir tu Plan</h1>
+    );
+}
+
+export default Planes;
