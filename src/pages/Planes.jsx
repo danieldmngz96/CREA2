6b0721e62361a38605ad32ae0f91377c40925f69
danieldmngz96 <danarturodo@hotmail.com>
2021-05-19 18:35:38 -0500

    elaboration of plans 35%

--- src/pages/Planes.jsx
+++ src/pages/Planes.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 //import PriceCard from '../components/PriceCard';
 //import Hero from '../containers/Hero';
-//import '../styles/pages/home.css';
+import '../styles/pages/planes.css';
 
 const Planes = () => {
     return (
-       <h1>Solo te falta un Paso <br/> 
-       para adquirir tu Plan</h1>
+    <div>
+   
+    <h1 className='title__one'>Solo te falta un Paso <br/> 
+    para adquirir tu Plan</h1>
+
+    <section className='box__main' >
+    <form className="position__input"> 
+    <label>
+     <input type="text" placeholder="Nombre"  />
+      </label>
+
+      <label>
+     <input type="text" placeholder="Usuario"  />
+      </label>
+      
+      <label>
+     <input type="password" placeholder="Password"  />
+      </label>
+
+      <label>
+     <input type="text" />
+      </label>
+    </form>
+    </section>
+    </div>
+      
+    
+  
     );
 }
 
