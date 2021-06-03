1adde5d63d37c570ce1c3427034bb17c7da186bd
FabianVegaA <fabianveal@gmail.com>
2021-06-02 21:45:48 -0400

    Added models service and client, and marshmalloclibrary

--- Flask/project/models/Client.py
+++ Flask/project/models/Client.py
@@ -1,18 +1,27 @@
-from flask_login import UserMixin
-from . import db
+from Flask.project.__init__ import db, ma
 
 
-class Client(UserMixin, db.Model):
-    pk_idClient = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    telephone = db.Column(db.String(11), unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(45), nullable=False)
-    user = db.Column(db.String(45), unique=True, nullable=False)
-    active = db.Column(db.Boolean, nullable=False)
+class ClientModel(db.Model):
+    __tablename__ = "Plan"
+    PK_ID_PLAN = db.Column(db.Integer, primary_key=True)
+    EMAIL = db.Column(db.String(100), unique=True, nullable=False)
+    TELEPHONE = db.Column(db.String(11), unique=True)
+    PASSWORD = db.Column(db.String(100), nullable=False)
+    NAME = db.Column(db.String(45), nullable=False)
+    USER = db.Column(db.String(45), unique=True, nullable=False)
+    ACTIVE = db.Column(db.Boolean, nullable=False)
 
-    fk_idPlan = db.relationship(
-        'pk_idPlan',
-        lazy='select',
-        backref=db.backref('Client', lazy='joined')
-    )
+    FK_ID_PLAN = db.Column(db.Integer, db.ForeignKey("Plan.pk_idPlan"))
+
+    def __repr__(self):
+        return "<{}:{}:{}>".format(self.PK_ID_PLAN, self.USER, self.ACTIVE)
+
+
+class ClientSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = ClientModel
+        include_fk = True
+
+    @post_load
+    def make_plan(self, data, **kwargs):
+        return ClientModel(**data)
