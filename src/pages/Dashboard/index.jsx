219cdce6dba298850051d31922855495ab83f262
danieldmngz96 <danarturodo@hotmail.com>
2021-06-14 16:40:42 -0500

    boton de whatapp
    boton de what app

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../../styles/pages/Dashboard/index.css';
+
 
 const Dashboard = () => {
   return (
     <section>
-      <h1>Dashboard home</h1>
+      <h1>Bienvenido a CREA DESARROLLO </h1>
+      <div className="container">
+        <div className="calendar">
+        <p>probando</p>
+        </div>
+        <div className="pay">
+        <a href="https://wa.me/?text=Me%20gustaría%20saber%20más%20sobre%20mi%20proyecto" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
+        </div>
+      </div>
     </section>
   );
 }
