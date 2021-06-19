1aa16ca46a19bb29b6aafd9202b33248ccac63ad
danieldmngz96 <danarturodo@hotmail.com>
2021-06-19 12:27:03 -0500

    calendar e index
    main calendar he index

--- src/pages/Dashboard/Schedule.jsx
+++ src/pages/Dashboard/Schedule.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../../styles/pages/Dashboard/Schedule.css';
+import CalendarBig from '../../components/CalendarBig';
 
 const Schedule = () => {
   return (
     <section>
-      <h1>Schedule</h1>
+      <h1>Calendario</h1>
+      <div className="position__CalendarBig">
+        <h4>Aqui podras encontrar todas las actividades:</h4>
+      <CalendarBig></CalendarBig>
+      </div>
       <a href="https://wa.me/?text=Me%20gustaría%20saber%20más%20sobre%20mi%20proyecto" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
     </section>
   );
